78727d5118aa83d138c46020cd129bbf5dd74e06
King <mac@mac.com>
2023-07-03 12:33:52 +0900

    bottom nav

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -3,15 +3,40 @@ import 'package:flutter/material.dart';
 class DefaultLayout extends StatelessWidget {
   final Widget child;
 
-  Color? backgroundColor;
+  final Color? backgroundColor;
+  final String? title;
+  final Widget? bottomNavigationBar;
 
-  DefaultLayout({super.key, required this.child, this.backgroundColor});
+  DefaultLayout(
+      {super.key,
+      required this.child,
+      this.backgroundColor,
+      this.title,
+      this.bottomNavigationBar});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor ?? Colors.white,
+      appBar: renderAppbar(),
+      bottomNavigationBar: bottomNavigationBar,
       body: child,
     );
+  }
+
+  AppBar? renderAppbar() {
+    if (title == null) {
+      return null;
+    } else {
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          title!,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+        ),
+        foregroundColor: Colors.black,
+      );
+    }
   }
 }
