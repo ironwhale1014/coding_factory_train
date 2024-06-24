404a017847138e6d3f3ed523653c0884e64c417c
King <mac@mac.com>
2024-06-25 06:40:18 +0900

    시작하는 기본 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:coding_factory_train/common/provider/go_router_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,16 +5,14 @@ void main() {
   runApp(const ProviderScope(child: _App()));
 }
 
-class _App extends ConsumerWidget {
+class _App extends StatelessWidget {
   const _App({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final router = ref.watch(routerProvider);
+  Widget build(BuildContext context) {
     return MaterialApp.router(
       theme: ThemeData(useMaterial3: true, fontFamily: 'NotoSans'),
       debugShowCheckedModeBanner: false,
-      routerConfig: router,
     );
   }
 }
