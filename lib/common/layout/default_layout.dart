33d9086359aeec2b3e7ce27766cdbbbe6d311026
King <mac@mac.com>
2024-06-26 22:04:57 +0900

    tabBarView, homePage end

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 
 class DefaultLayout extends StatelessWidget {
   const DefaultLayout(
-      {super.key, required this.child, this.backgroundColor = Colors.white});
+      {super.key, required this.child, this.backgroundColor = Colors.white, this.bottomNavigationBar});
 
   final Widget child;
   final Color backgroundColor;
+  final Widget? bottomNavigationBar;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
+      bottomNavigationBar: bottomNavigationBar,
       body: child,
     );
   }
--- lib/common/view/root_tap.dart
+++ lib/common/view/root_tap.dart
@@ -1,14 +1,64 @@
 import 'package:coding_factory_train/common/layout/default_layout.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class RootTap extends StatelessWidget {
+class RootTap extends ConsumerStatefulWidget {
   const RootTap({super.key});
 
   @override
+  ConsumerState createState() => _RootTapState();
+}
+
+class _RootTapState extends ConsumerState<RootTap>
+    with SingleTickerProviderStateMixin {
+  int _selectedIndex = 0;
+  late TabController controller;
+
+  final List<NavigationDestination> _taps = [
+    const NavigationDestination(icon: Icon(Icons.home), label: "home"),
+    const NavigationDestination(
+        icon: Icon(Icons.fastfood_outlined), label: "food"),
+    const NavigationDestination(icon: Icon(Icons.receipt_long), label: "order"),
+    const NavigationDestination(
+        icon: Icon(Icons.person_2_outlined), label: "profile"),
+  ];
+
+  final List<Widget> _screens = [
+    Container(color: Colors.red),
+    Container(color: Colors.green),
+    Container(color: Colors.blue),
+    Container(color: Colors.purple),
+  ];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = TabController(length: 4, vsync: this);
+    controller.addListener(tabBarListener);
+  }
+
+  void tabBarListener() {
+    setState(() {
+      _selectedIndex = controller.index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const DefaultLayout(
-        child: Center(
-      child: Text("root_Tap"),
-    ));
+    return DefaultLayout(
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: _selectedIndex,
+          onDestinationSelected: (int index) {
+            controller.animateTo(index);
+          },
+          destinations: _taps,
+        ),
+        child: TabBarView(
+          physics: const NeverScrollableScrollPhysics(),
+          controller: controller,
+          children: _screens,
+        ));
   }
 }
